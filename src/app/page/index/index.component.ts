7b81d37e74c5fd252c543141f5c3192ebbb5fc47
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-08-29 08:11:57 -0300

    button-index css

--- src/app/page/index/index.component.ts
+++ src/app/page/index/index.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../component/header/header.component";
+import { ButtonIndexComponent } from "../../component/button-index/button-index.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ButtonIndexComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })

35a5fbf9c834b9b798c0c12412a01c37611f3958
Andrioh <121702052+Andrioh@users.noreply.github.com>
2024-08-31 00:49:29 -0300

    organization-component 1/2

--- src/app/component/organizations-component/organizations-component.component.ts
+++ src/app/component/organizations-component/organizations-component.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-organizations-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './organizations-component.component.html',
   styleUrl: './organizations-component.component.css'
 })
 export class OrganizationsComponentComponent {
 
+  HasOrganization: boolean = false;
+
+  StartOrganization(){
+
+  }
 }
--- src/app/page/organizations/organizations.component.ts
+++ src/app/page/organizations/organizations.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponentModule } from "../../component/header/header.module";
+import { OrganizationsComponentComponent } from "../../component/organizations-component/organizations-component.component";
 
 @Component({
   selector: 'app-organizations',
   standalone: true,
-  imports: [HeaderComponentModule],
+  imports: [HeaderComponentModule, OrganizationsComponentComponent],
   templateUrl: './organizations.component.html',
   styleUrl: './organizations.component.css'
 })

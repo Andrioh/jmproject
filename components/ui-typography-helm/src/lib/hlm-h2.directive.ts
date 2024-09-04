f2653a837c67588ff28b8102487cde4baf3271ea
jonathanqmm <jonathanqmm@>
2024-09-04 20:46:52 -0300

    fix: fix sparta style for h2

--- components/ui-typography-helm/src/lib/hlm-h2.directive.ts
+++ components/ui-typography-helm/src/lib/hlm-h2.directive.ts
@@ -2,9 +2,7 @@ import { Directive, computed, input } from '@angular/core';
 import { hlm } from '@spartan-ng/ui-core';
 import type { ClassValue } from 'clsx';
 
-export const hlmH2 =
-	'scroll-m-20 border-border border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0';
-
+export const hlmH2 = 'scroll-m-20 text-3xl font-semibold tracking-tight';
 @Directive({
 	selector: '[hlmH2]',
 	standalone: true,

719789ba1c9e866c50fb2b7c5bd2b8fc9776fefd
jonathanqmm <jonathanqmm@>
2024-09-04 21:13:45 -0300

    refactor: removed unused styles and applied flex to the main container to keep it full screen when required

--- src/app/component/organizations-component/organizations-component.component.ts
+++ src/app/component/organizations-component/organizations-component.component.ts
@@ -1,76 +1,96 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HlmTableModule,} from '@spartan-ng/ui-table-helm';
-import { HeaderComponent } from '../header/header.component';
-import { HeaderService } from '../../Service/header.service';
+import { Component } from '@angular/core';
 import { provideIcons } from '@ng-icons/core';
 import { lucideArrowLeft } from '@ng-icons/lucide';
-import { HlmButtonModule } from '@spartan-ng/ui-button-helm';
+import { HlmButtonDirective, HlmButtonModule } from '@spartan-ng/ui-button-helm';
 import { HlmIconModule } from '@spartan-ng/ui-icon-helm';
 import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
-import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmTableModule, } from '@spartan-ng/ui-table-helm';
+import { HeaderService } from '../../Service/header.service';
+import { HlmTTypographyModule } from '../../sparta-ext/typograph..module';
+import { HeaderComponent } from '../header/header.component';
 
 
 
 interface organization {
-  id: number,
-  name: string,
+    id: number,
+    name: string,
 }
 
 @Component({
-  selector: 'app-organizations-component',
-  standalone: true,
-  imports: [CommonModule, HlmTableModule, HeaderComponent, HlmButtonModule, HlmIconModule, HlmLabelDirective, HlmButtonDirective],
-  providers: [provideIcons({lucideArrowLeft})],
-  templateUrl: './organizations-component.component.html',
-  styleUrl: './organizations-component.component.css'
+    selector: 'app-organizations-component',
+    standalone: true,
+    imports: [
+        CommonModule,
+        HeaderComponent,
+
+        //Spartan
+        HlmTTypographyModule,
+        HlmTableModule,
+        HlmButtonModule,
+        HlmIconModule,
+        HlmLabelDirective,
+        HlmButtonDirective
+    ],
+    providers: [provideIcons({ lucideArrowLeft })],
+    templateUrl: './organizations-component.component.html',
+    styleUrl: './organizations-component.component.css',
+    host: {
+        class: "flex flex-col flex-1"
+    }
 })
 
 export class OrganizationsComponentComponent {
 
-  constructor(private headerservice:HeaderService){}
+    constructor(private headerservice: HeaderService) { }
 
-  organizations: organization[] = [];
+    organizations: organization[] = [];
 
-  HasOrganization: boolean = false;
-  HasCollaborate: boolean = false;
-  ColorBackground: string = "white";
-  StartOrganization() {
-    this.HasOrganization = !this.HasOrganization;
-  }
+    hasOrganization: boolean = false;
 
-  NwOrganization() {
-    if (this.organizations) {
-      let lastId = this.organizations.reduce((maxId, org) => Math.max(maxId, org.id), 0);
-
-      lastId += 1;
-
-      const format = lastId < 10 ? `0${lastId}` : `${lastId}`;
-
-      const newOrga = { id: lastId, name: `Organization ${format}` };
-
-      this.organizations.push(newOrga)
-    }
-  }
-
-  EditOrga: boolean = false;
-
-  EditOrganization() {
-
-    if (this.EditOrga == false) {
-      this.EditOrga = true
-      this.ColorBackground = "#E5E7EB"
-      this.headerservice.AlterColor()
-    } else {
-      this.EditOrga = false
-      this.ColorBackground = "white"
-      this.headerservice.AlterColor()
+    create() {
+        this.hasOrganization = !this.hasOrganization;
     }
 
-  }
 
-  FinishEdit(){
-    this.EditOrganization()
-  }
+
+
+
+    HasCollaborate: boolean = false;
+    ColorBackground: string = "white";
+
+    NwOrganization() {
+        if (this.organizations) {
+            let lastId = this.organizations.reduce((maxId, org) => Math.max(maxId, org.id), 0);
+
+            lastId += 1;
+
+            const format = lastId < 10 ? `0${lastId}` : `${lastId}`;
+
+            const newOrga = { id: lastId, name: `Organization ${format}` };
+
+            this.organizations.push(newOrga)
+        }
+    }
+
+    EditOrga: boolean = false;
+
+    EditOrganization() {
+
+        if (this.EditOrga == false) {
+            this.EditOrga = true
+            this.ColorBackground = "#E5E7EB"
+            this.headerservice.AlterColor()
+        } else {
+            this.EditOrga = false
+            this.ColorBackground = "white"
+            this.headerservice.AlterColor()
+        }
+
+    }
+
+    FinishEdit() {
+        this.EditOrganization()
+    }
 
 }
